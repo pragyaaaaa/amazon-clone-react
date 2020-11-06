f440e984172437226cf208a692e5349e150a57ff
pragyaaaaa <pragyasabstracts@gmail.com>
2020-11-06 20:08:37 +0530

    --firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhCwXnn2LqqKX7VEOuEQBfItcZz2_xn6A",
+    authDomain: "clone-e5240.firebaseapp.com",
+    databaseURL: "https://clone-e5240.firebaseio.com",
+    projectId: "clone-e5240",
+    storageBucket: "clone-e5240.appspot.com",
+    messagingSenderId: "517236636456",
+    appId: "1:517236636456:web:3a23aef3595ec9daaa2e1d",
+    measurementId: "G-DP7Z0NPHYJ"
+};
