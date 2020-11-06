18ac77fef26185ffa10b61a8e4bf28298708e19a
pragyaaaaa <pragyasabstracts@gmail.com>
2020-11-06 23:36:52 +0530

    --using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      {/*Header*/}
-      {/*Home*/}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Trying the router. It works if you're seeing this.</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
