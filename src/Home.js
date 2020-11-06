18f2921efc58758c24f48001a95fa1c8de07754e
pragyaaaaa <pragyasabstracts@gmail.com>
2020-11-06 21:22:56 +0530

    --home-page: partially complete.

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home-container">
+                <img className="home-image"
+                    src="https://imgk.timesnownews.com/story/1568022305-amazon-great-indian-festival-Sept2019-banner.jpg"
+                    alt="banner">
+                </img>
+            </div>
+            <div className="home-row">
+                {/* product */}
+                
+                {/* product */}
+            </div>
+            <div className="home-row">
+                {/* product */}
+                {/* product */}
+                {/* product */}
+            </div>
+
+            <div className="home-row">
+                {/* product */}
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
+
